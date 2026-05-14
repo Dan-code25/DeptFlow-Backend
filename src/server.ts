cf6406363658660796f0b04cb4b2aec9f07571a1
Dan-code25 <danjhenielbringas1125@gmail.com>
2026-05-15 02:02:19 +0800

    Update(package.json, server.ts, tsconfig.json): Refactor CORS middleware, add build and start scripts, and adjust TypeScript configuration

--- src/server.ts
+++ src/server.ts
@@ -34,7 +34,15 @@ const PORT = process.env.PORT || 3000;
 
 
 // Middleware
-app.use(cors({ credentials: true, origin: "*" }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://your-future-frontend-url.vercel.app",
+    ],
+    credentials: true,
+  }),
+);
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
